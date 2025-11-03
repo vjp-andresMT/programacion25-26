be38717f3c384d800790678e4e40d746e9d0e7bf
vjp-andresMT <amohedanot01@informatica.iesvalledeljerteplasencia.es>
2025-11-03 17:00:23 +0100

    ejerciciosTema1

---
+++ tema1pro/Ejercicio3.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio3;
+
+/**
+ *
+ * @author andres
+ */
+public class Ejercicio3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         
+
+   
+    System.out.println("Bienvenido al mundo de java.");
+    System.out.println("el camino sera duro, pero merecera la pena");
+}
+}
+
+    
+    
+
